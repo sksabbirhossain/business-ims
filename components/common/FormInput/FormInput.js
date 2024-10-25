3f26f632e8981747b5f5ce43ed505377f53a1f69
sksabbirhossain <sksabbirhossain4@gmail.com>
2024-10-25 16:40:22 +0600

    created return sales page

--- components/common/FormInput/FormInput.js
+++ components/common/FormInput/FormInput.js
@@ -12,7 +12,7 @@ const FormInput = ({ label, type, name, placeholder, value, ...rest }) => {
         value={value}
         name={name}
         {...rest}
-        className="w-full rounded-[3px] px-1.5 py-2 text-sm ring-1 ring-primary focus:outline-none focus:ring-green-700"
+        className="w-full rounded-[3px] px-1.5 py-2 text-sm ring-1 ring-primary placeholder:capitalize focus:outline-none focus:ring-green-700"
       />
     </div>
   );

febcae47464307bd2fd946a15ce57c53c7dae2fb
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-02-28 15:23:56 +0600

    showing purchase details data in details purchase page

--- app/admin/purchase-details/[id]/page.js
+++ app/admin/purchase-details/[id]/page.js
@@ -1,9 +1,108 @@
-import React from 'react'
+import { getPurchase } from "@/actions/storeAdmin/purchase/purchaseActions";
+import Container from "@/components/common/Container/Container";
+import PageHeader from "@/components/common/PageHeader/PageHeader";
+import React from "react";
 
-const PurchaseDetails = () => {
+export const metadata = {
+  title: "Purchase Details",
+};
+
+const PurchaseDetails = async ({ params }) => {
+  const purchaseId = (await params).id;
+  const purchase = await getPurchase(purchaseId);
   return (
-    <div>PurchaseDetails</div>
-  )
-}
+    <Container>
+      <PageHeader headText="Stock Details" />
+      {/* details */}
+      <div className="rounded-lg bg-white p-4 shadow-md">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+          <div className="space-y-3">
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Product Name</h2>
+              <p className="text-gray-800">{purchase?.data?.name}</p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">product description</h2>
+              <p className="text-gray-800">{purchase?.data?.description}</p>
+            </div>
+            <div className="space-y-3 rounded shadow shadow-primary">
+              <div className="space-y-1 rounded p-3 shadow">
+                <h2 className="text-md font-semibold">Category</h2>
+                <p className="text-gray-800">
+                  {purchase?.data?.category?.name}
+                </p>
+              </div>
+              <div className="space-y-1 rounded p-3 shadow">
+                <h2 className="text-md font-semibold">Category Description</h2>
+                <p className="text-gray-800">
+                  {purchase?.data?.category?.description}
+                </p>
+              </div>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Unit of Measure</h2>
+              <p className="text-gray-800">{purchase?.data?.uom}</p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Purchase Price</h2>
+              <p className="text-gray-800">
+                {purchase?.data?.purchasePrice} Taka
+              </p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Selling Price</h2>
+              <p className="text-gray-800">
+                {purchase?.data?.sellingPrice} Taka
+              </p>
+            </div>
+          </div>
 
-export default PurchaseDetails
+          <div className="space-y-3">
+            <div className="space-y-3 rounded shadow shadow-primary">
+              <div className="space-y-1 rounded p-3 shadow">
+                <h2 className="text-md font-semibold">Supplier</h2>
+                <p className="text-gray-800">
+                  {purchase?.data?.supplierInfo?.name}
+                </p>
+              </div>
+              <div className="space-y-1 rounded p-3 shadow">
+                <h2 className="text-md font-semibold">Supplier Shop Name</h2>
+                <p className="text-gray-800">
+                  {purchase?.data?.supplierInfo?.shopName}
+                </p>
+              </div>
+              <div className="space-y-1 rounded p-3 shadow">
+                <h2 className="text-md font-semibold">
+                  Supplier Contact Number
+                </h2>
+                <p className="text-gray-800">
+                  {purchase?.data?.supplierInfo?.phone}
+                </p>
+              </div>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Quantity</h2>
+              <p className="text-gray-800">{purchase?.data?.quantity}</p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">SKU</h2>
+              <p className="text-gray-800">{purchase?.data?.sku}</p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Bar Code</h2>
+              <p className="text-gray-800">{purchase?.data?.barcode}</p>
+            </div>
+            <div className="space-y-1 rounded p-3 shadow">
+              <h2 className="text-md font-semibold">Status</h2>
+              <p className="text-gray-800">
+                {purchase?.data?.status ? "Active" : "Inactive"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default PurchaseDetails;

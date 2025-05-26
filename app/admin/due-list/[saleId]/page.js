f23ab2a8603dd95c314fd2ebd452b598263187c3
sksabbirhossain <sksabbirhossain4@gmail.com>
2025-05-26 22:00:28 +0600

    created dueSale Payment handler

--- app/admin/due-list/[saleId]/page.js
+++ app/admin/due-list/[saleId]/page.js
@@ -6,13 +6,19 @@
  *Contact Info: https://www.linkedin.com/in/sk-sabbir-hossain
  */
 
+import { getSale } from "@/actions/storeAdmin/sales/salesActions";
 import DuePaymentForm from "@/components/admin/Due/DuePaymentForm";
 import Container from "@/components/common/Container/Container";
 import PageHeader from "@/components/common/PageHeader/PageHeader";
 
+export const metadata = {
+  title: "Due Payment",
+  description: "Add due payment for a sale",
+};
+
 const DuePayment = async ({ params }) => {
-  const { dueId } = await params;
-  // const duePayment = await getDuePayment(dueId);
+  const { saleId } = await params;
+  const dueSale = await getSale(saleId);
 
   return (
     <Container>
@@ -22,7 +28,7 @@ const DuePayment = async ({ params }) => {
       <div className="flex h-full min-h-[75vh] w-full items-center justify-center rounded-md bg-white/50 p-4 px-2 py-5 shadow-sm shadow-primary backdrop-blur">
         <div className="w-full max-w-[400px] rounded p-5 shadow">
           {/* form componet */}
-          <DuePaymentForm />
+          <DuePaymentForm dueSale={dueSale} />
         </div>
       </div>
     </Container>
--- components/admin/Due/DuePaymentForm.js
+++ components/admin/Due/DuePaymentForm.js
@@ -10,37 +10,100 @@
 
 import Button from "@/components/common/Button/Button";
 import FormInput from "@/components/common/FormInput/FormInput";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
-const DuePaymentForm = () => {
-  const [ammount, setAmmount] = useState("");
+const DuePaymentForm = ({ dueSale }) => {
+  const [amount, setAmount] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
+
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setErrors({});
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/admin/sale/due-sales-payment`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ amount, saleId: dueSale?.data?._id }),
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session?.user?.accessToken}`,
+          },
+        },
+      );
+      const data = await res.json();
+
+      if (data?.data?._id) {
+        setLoading(false);
+        toast.success("Due Added Successful!");
+        router.push("/admin/due-list");
+      } else {
+        setLoading(false);
+        setErrors(data);
+      }
+    } catch (err) {
+      setLoading(false);
+      setErrors({
+        errors: {
+          common: {
+            // msg: err.message,
+            msg: "Intranal server error!",
+          },
+        },
+      });
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="space-y-3">
         <div className="flex h-full w-full justify-between gap-4 border-b pb-2">
           <p className="text-sm font-medium capitalize">Customer Name</p>
-          <p className="text-sm font-semibold">Sk Sabbir Hossain</p>
+          <p className="text-sm font-semibold">
+            {dueSale?.data?.customer?.name || "N/A"}
+          </p>
         </div>
         <div className="flex h-full w-full justify-between gap-4 border-b pb-2">
           <p className="text-sm font-medium capitalize">Transaction id</p>
-          <p className="text-sm font-semibold">Sk-dsf</p>
+          <p className="text-sm font-semibold">{dueSale?.data?.trxid}</p>
         </div>
         <div className="flex h-full w-full justify-between gap-4 border-b pb-2">
           <p className="text-sm font-medium capitalize">total price</p>
-          <p className="text-sm font-semibold">34343 Tk.</p>
+          <p className="text-sm font-semibold">
+            {dueSale?.data?.totalPrice} Tk.
+          </p>
         </div>
         <div className="flex h-full w-full justify-between gap-4 border-b pb-2">
           <p className="text-sm font-medium capitalize">total due</p>
-          <p className="text-sm font-semibold">43243 Tk.</p>
+          <p className="text-sm font-semibold">{dueSale?.data?.due} Tk.</p>
         </div>
         <FormInput
           type="number"
-          placeholder="Enter Your Payment Ammount"
-          name="ammount"
-          value={ammount}
-          onChange={(e) => setAmmount(e.target.value)}
+          placeholder="Enter Your Payment amount"
+          name="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
         />
-        <Button className="w-full">Add</Button>
+        <Button className="w-full" isPending={loading}>
+          Add
+        </Button>
+        {errors?.errors?.common && (
+          <p className="rounded bg-red-600 py-2 text-center text-sm font-medium text-white">
+            {errors?.errors?.common?.msg}
+          </p>
+        )}
       </div>
     </form>
   );
